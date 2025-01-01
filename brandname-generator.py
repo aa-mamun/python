77f2b68652fa2a370032c9323d60cba4c1f3591a
Abdullah Al Mamun <mamun.ofcpro@gmail.com>
2025-01-01 23:36:02 +0600

    Initial commit

---
+++ brandname-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand name generator app !!! ")
+city = input("What's the name of your city where you grow up ? \n")
+pet = input("What's the name of your favourite pet ? \n")
+print(f"So, your brand name is : {city.upper() +" "+ pet.lower()}")
